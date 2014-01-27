e28b484ab2c3f29f5b7b8a507740dbd30a92b910
EliteTK <tomasz.kramkowski@gmail.com>
2014-01-27 18:08:51 +0000

    Fixed a bunch of things.

--- lua/darkrp_customthings/jobs.lua
+++ lua/darkrp_customthings/jobs.lua
@@ -321,7 +321,7 @@ TEAM_HOBO = DarkRP.createJob("Hobo", {
      })
      
      TEAM_TECH = DarkRP.createJob("Technician", {
-        color = Color(, 160, 200, 255),
+        color = Color(160, 200, 255),
         model = "models/player/eli.mdl",
         description = [[You are to build wire contraptions and sell them to players for security and other purposes. No entity finders or abusing.]],
         weapons = {},
