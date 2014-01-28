0d31e9bac9279cdd0528f9ea7860cd66f04fc781
markrjg <mark.annable@hotmail.co.uk>
2014-01-28 12:54:32 +0000

    Update motdtab.lua

--- lua/darkrp_modules/extraf4tab/motdtab.lua
+++ lua/darkrp_modules/extraf4tab/motdtab.lua
@@ -1,1 +1,11 @@
 -- new file
+local url = "http://bringvictory.com/"
+local tabName = "MOTD"
+
+
+local function createF4MenuTab()
+	local webPage = vgui.Create("F1HTML")
+	webPage:OpenURL(url)
+	DarkRP.addF4MenuTab(tabName, webPage)
+end
+hook.Add("F4MenuTabs", "MyCustomF4MenuTab", createF4MenuTab)
