cd0091b3d9201a524baf2ba3f7cb998ca70b512b
markrjg <mark.annable@hotmail.co.uk>
2014-01-28 12:55:06 +0000

    Update donationtab.lua

--- lua/darkrp_modules/extraf4tab/donationtab.lua
+++ lua/darkrp_modules/extraf4tab/donationtab.lua
@@ -1,1 +1,10 @@
--- new file
+local url = "http://bringvictory.com/"
+local tabName = "Donations"
+
+
+local function createF4MenuTab()
+	local webPage = vgui.Create("F1HTML")
+	webPage:OpenURL(url)
+	DarkRP.addF4MenuTab(tabName, webPage)
+end
+hook.Add("F4MenuTabs", "MyCustomF4MenuTab", createF4MenuTab)
